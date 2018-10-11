37b5450ded71f212b861c8b2b7282e1a8eee21d8
麦广扬 <maiguangyang@163.com>
2018-10-11 16:48:01 +0800

    aes对称解密

--- public/rsa.go
+++ public/rsa.go
@@ -1,57 +1,76 @@
 package public
 
 import (
+  // "fmt"
   "crypto/rsa"
   "crypto/rand"
   "crypto/x509"
   "encoding/pem"
   "encoding/base64"
   "encoding/json"
+  "encoding/hex"
+  "crypto/cipher"
+  "crypto/aes"
   "bytes"
   "errors"
   // Utils "../utils"
 )
 
-var prvKey *rsa.PrivateKey
-var PubPemEnc string
+const (
+  aesKey = "!IWCT$C6hSA5iVQ8"
+)
 
-// 生成rsa key
-func Gen() error {
-  bits := int(1024)
-  var err error
-
-  prvKey, err = rsa.GenerateKey(rand.Reader, bits)
+func AesDecrypt(ciphertext, key []byte) ([]byte, error) {
+  pkey := PaddingLeft(key, '0', 16)
+  block, err := aes.NewCipher(pkey) //选择加密算法
   if err != nil {
-    return err
+    return nil, err
   }
 
-  pkix, err := x509.MarshalPKIXPublicKey(&prvKey.PublicKey)
-  if err != nil {
-    return err
-  }
-
-  block := pem.Block{
-    Type: "PUBLIC KEY",
-    Bytes: pkix,
-  }
-
-  pubPem := pem.EncodeToMemory(&block)
-  PubPemEnc = base64.StdEncoding.EncodeToString(pubPem)
-
-  return nil
+  blockModel := cipher.NewCBCDecrypter(block, pkey)
+  plantText := make([]byte, len(ciphertext))
+  blockModel.CryptBlocks(plantText, []byte(ciphertext))
+  plantText = PKCS7UnPadding(plantText, block.BlockSize())
+  return plantText, nil
 }
+
+func PKCS7UnPadding(plantText []byte, blockSize int) []byte {
+  length := len(plantText)
+  unpadding := int(plantText[length-1])
+  return plantText[:(length - unpadding)]
+}
+
+func PaddingLeft(ori []byte, pad byte, length int) []byte {
+  if len(ori) >= length {
+    return ori[:length]
+  }
+  pads := bytes.Repeat([]byte{pad}, length-len(ori))
+  return append(pads, ori...)
+}
+
 
 // 解密
 func Decrypt(text, prvPemEnc string) ([]byte, error) {
   var ciphertext []byte
 
+  // 先base64解密
   prvPem, err := base64.StdEncoding.DecodeString(prvPemEnc)
+
+  // 再使用hex.DecodeString转换
+  dexDecode, err := hex.DecodeString(string(prvPem))
+
   if err != nil {
     return ciphertext, err
   }
+
+  // 最后解密对称加密的内容
+  prvPem, err = AesDecrypt(dexDecode, []byte(aesKey))
+  if err != nil {
+    return ciphertext, err
+  }
+
+  // 公私钥加密
   block, _ := pem.Decode(prvPem)
-
-
   if block == nil {
     return ciphertext, errors.New("failed to parse PEM block containing the private key")
   }
@@ -101,11 +120,23 @@ func DecryptJson(text, pubPemEnc string) (map[string]interface{}, error) {
 // 加密
 func Encrypt(text, pubPemEnc string) (string, error) {
   var encrypted string
+  // 先base64解密
   pubPem, err := base64.StdEncoding.DecodeString(pubPemEnc)
+
+  // 再使用hex.DecodeString转换
+  dexDecode, err := hex.DecodeString(string(pubPem))
+
   if err != nil {
     return encrypted, err
   }
 
+  // 最后解密对称加密的内容
+  pubPem, err = AesDecrypt(dexDecode, []byte(aesKey))
+  if err != nil {
+    return encrypted, err
+  }
+
+  // 公私钥加密
   block, _ := pem.Decode(pubPem)
   if block == nil {
     return encrypted, errors.New("failed to parse PEM block containing the public key")
